39d562f2f874a74bdc80025bd1b4813c2ceca53b
Thomas <thomasmillergb@outlook.com>
2016-04-23 12:49:49 +0100

    TPMG-2 Create Salary Object

--- src/main/java/org/thomasmillergb/thepropertymarketgame/ThePropertyMarketGameApplication.java
+++ src/main/java/org/thomasmillergb/thepropertymarketgame/ThePropertyMarketGameApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ThePropertyMarketGameApplication {
-    //test
     public static void main(String[] args) {
         SpringApplication.run(ThePropertyMarketGameApplication.class, args);
     }
