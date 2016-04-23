807e27c4365190853f75d8e0f8ca0104dab11fe7
Thomas Miller <Thomas Miller>
2016-04-23 11:08:40 +0100

    Initial commit

---
+++ src/main/java/org/thomasmillergb/thepropertymarketgame/ThePropertyMarketGameApplication.java
@@ -0,0 +1,12 @@
+package org.thomasmillergb.thepropertymarketgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePropertyMarketGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThePropertyMarketGameApplication.class, args);
+	}
+}
---
+++ src/test/java/org/thomasmillergb/thepropertymarketgame/ThePropertyMarketGameApplicationTests.java
@@ -0,0 +1,16 @@
+package org.thomasmillergb.thepropertymarketgame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThePropertyMarketGameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
