756aa525f8cdde56abb6fb4ec2e5b2869f433f9f
Thomas <thomasmillergb@outlook.com>
2016-04-23 12:32:03 +0100

    #TPMG-5 test

--- src/main/java/org/thomasmillergb/thepropertymarketgame/ThePropertyMarketGameApplication.java
+++ src/main/java/org/thomasmillergb/thepropertymarketgame/ThePropertyMarketGameApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ThePropertyMarketGameApplication {
 
+    //TPMG-5 test #TPMG-5 test
     public static void main(String[] args) {
         SpringApplication.run(ThePropertyMarketGameApplication.class, args);
     }
